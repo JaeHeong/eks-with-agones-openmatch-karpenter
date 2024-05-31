a2b281a81218c66a57341bd01483be0ce48ffe0c
EC2 Default User <ec2-user@ip-172-31-27-218.ec2.internal>
2024-05-31 09:29:06 +0000

    still not working karpenter... taging?

--- terraform/cluster/modules/cluster/main.tf
+++ terraform/cluster/modules/cluster/main.tf
@@ -201,7 +201,7 @@ module "vpc" {
   }
 
   private_subnet_tags = {
-   "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb" = 1
     "karpenter.sh/discovery"              = var.cluster_name
   }
